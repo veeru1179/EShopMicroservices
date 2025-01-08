845f0422ce54f76565ab8e98bbc632a2bdfa8b7d
Golakaram Prasad <Golakaram.Prasad@odessainc.com>
2025-01-08 15:02:38 +0530

    product code

---
+++ eshop.microservices/src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+ public interface ICommandHandler<in TCommnad> 
+        : ICommandHandler<TCommnad, Unit> 
+        where TCommnad:ICommand<Unit>
+    { }
+    public interface ICommandHandler<in TCommnad,TResponse>
+        :IRequestHandler<TCommnad, TResponse>
+        where TCommnad:ICommand<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
---
+++ eshop.microservices/src/Services/CatalogAPI/Products/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,27 @@
+﻿ 
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid ResultId);
+    public class CreateProductEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var commnad = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(commnad);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.ResultId}", response);
+            })
+             .WithName("CreateProduct")
+             .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+             .ProducesProblem(StatusCodes.Status400BadRequest)
+             .WithSummary("Create Product")
+             .WithDescription("Create Product");
+
+        }
+    }
+}
